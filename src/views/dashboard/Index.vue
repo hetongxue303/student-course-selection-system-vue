fa0a04361c1154b16a5c43358abcdb55466df3e9
hetongxue303 <1974597686@qq.com>
2023-01-14 16:50:22 +0800

    feat: 完善首页布局

--- src/views/dashboard/Index.vue
+++ src/views/dashboard/Index.vue
@@ -1,9 +1,12 @@
 <template>
+  <div class="logo">
+    <el-image src="" alt="" />
+  </div>
   <div class="dashboard">
     <div
       class="card"
       style="
-        background-image: linear-gradient(to right, #4facfe 0%, #00f2fe 100%);
+        background-image: linear-gradient(to right, #004c8f 0%, #1ec2c2 100%);
       "
     >
       <svg-icon name="student-box" :size="60" unit="px" />
@@ -18,7 +21,7 @@
     <div
       class="card"
       style="
-        background-image: linear-gradient(to right, #43e97b 0%, #38f9d7 100%);
+        background-image: linear-gradient(120deg, #00ef5c 0%, #2dcbd0 100%);
       "
     >
       <svg-icon name="teacher-box" :size="60" unit="px" />
@@ -33,7 +36,7 @@
     <div
       class="card"
       style="
-        background-image: linear-gradient(120deg, #e0c3fc 0%, #8ec5fc 100%);
+        background-image: linear-gradient(120deg, #461b93 0%, #ac82d3 100%);
       "
     >
       <svg-icon name="major-box" :size="60" unit="px" />
@@ -48,7 +51,7 @@
     <div
       class="card"
       style="
-        background-image: linear-gradient(120deg, #f093fb 0%, #f5576c 100%);
+        background-image: linear-gradient(120deg, #a900bd 0%, #c5649b 100%);
       "
     >
       <svg-icon name="course-box" :size="60" unit="px" />
@@ -61,16 +64,10 @@
       </div>
     </div>
   </div>
-  <div
-    ref="refData"
-    style="
-      height: 300px;
-      width: 100%;
-      border-radius: 10px;
-      border: 1px solid #968e8e;
-      margin-top: 20px;
-    "
-  />
+  <div class="ref-box">
+    <div ref="choiceRef" style="height: 400px; width: 50%; margin-top: 30px" />
+    <div ref="courseRef" style="height: 400px; width: 50%; margin-top: 30px" />
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -78,6 +75,7 @@ import { getIndexInfo } from '../../api'
 import { onMounted, ref } from 'vue'
 import { IndexVo } from '../../types/entity'
 import { cloneDeep } from 'lodash'
+import moment from 'moment'
 import * as echarts from 'echarts'
 
 const info = ref<IndexVo>({})
@@ -93,18 +91,133 @@ onMounted(() => {
   initEchartsData()
 })
 
-const refData = ref<HTMLElement>()
+const choiceRef = ref<HTMLElement>()
+const courseRef = ref<HTMLElement>()
 const initEchartsData = () => {
-  echarts.init(refData.value!).setOption({
+  echarts.init(choiceRef.value!).setOption({
     title: {
-      text: '选课统计',
+      text: '近期选课',
       left: 'center'
+    },
+    xAxis: {
+      type: 'category',
+      axisTick: {
+        show: false
+      },
+      axisLine: {
+        show: false
+      },
+      data: [
+        moment(new Date()).format('YYYY-MM-DD'),
+        moment(new Date()).subtract(1, 'day').format('YYYY-MM-DD'),
+        moment(new Date()).subtract(2, 'day').format('YYYY-MM-DD'),
+        moment(new Date()).subtract(3, 'day').format('YYYY-MM-DD'),
+        moment(new Date()).subtract(4, 'day').format('YYYY-MM-DD'),
+        moment(new Date()).subtract(5, 'day').format('YYYY-MM-DD'),
+        moment(new Date()).subtract(6, 'day').format('YYYY-MM-DD')
+      ]
+    },
+    yAxis: {
+      type: 'value',
+      min: 0,
+      max: 50,
+      interval: 10,
+      axisTick: {
+        show: false
+      },
+      axisLine: {
+        show: false
+      }
+    },
+    series: {
+      name: '人数',
+      type: 'bar',
+      data: [15, 20, 5, 0, 17, 33, 22],
+      barWidth: '50%',
+      showBackground: true,
+      backgroundStyle: {
+        color: 'rgba(180, 180, 180, 0.2)'
+      }
+    },
+    tooltip: {
+      trigger: 'axis',
+      showDelay: 0,
+      axisPointer: {
+        type: 'shadow'
+      }
+    }
+  })
+  echarts.init(courseRef.value!).setOption({
+    title: {
+      text: '课程人数',
+      left: 'center'
+    },
+    xAxis: {
+      type: 'category',
+      axisTick: {
+        show: false
+      },
+      axisLine: {
+        show: false
+      },
+      data: ['JAVA实战', '物联网工程', '单片机及其应用', '高等数学', '大学英语']
+    },
+    yAxis: {
+      type: 'value',
+      min: 0,
+      max: 100,
+      interval: 10,
+      axisTick: {
+        show: false
+      },
+      axisLine: {
+        show: false
+      }
+    },
+    series: {
+      name: '人数',
+      type: 'bar',
+      data: [15, 45, 15, 8, 57],
+      barWidth: '50%',
+      showBackground: true,
+      backgroundStyle: {
+        color: 'rgba(180, 180, 180, 0.2)'
+      }
+    },
+    tooltip: {
+      trigger: 'axis',
+      showDelay: 0,
+      axisPointer: {
+        type: 'shadow'
+      }
     }
   })
 }
 </script>
 
 <style scoped lang="scss">
+.logo {
+  width: 100%;
+  height: 250px;
+  background: linear-gradient(
+    123deg,
+    #461b93 0%,
+    #461b93 40%,
+    #6a3cbc calc(40% + 1px),
+    #6a3cbc 60%,
+    #8253d7 calc(60% + 1px),
+    #8253d7 70%,
+    #f78f1e calc(70% + 1px),
+    #f78f1e 100%
+  );
+}
+
+.ref-box {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
 .dashboard {
   width: 100%;
   height: 80px;
